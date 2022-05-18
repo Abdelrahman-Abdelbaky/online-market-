e48b40f6cda5f2554f596005d5f74ad922269baa
abdelrhmanabdelbaky <86623558+abdelrhmanabdelbaky@users.noreply.github.com>
2022-05-18 02:56:57 +0200

    final

---
+++ Market/Controllers/UserInterfaces/ICheckout.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Market.Controllers.UserInterfaces
+{
+    public interface ICheckout
+    {
+        public void CheckZeroQuantity(int Quantity, int productId);
+        public IActionResult Checkout(int? Id);
+        public IActionResult CheckoutDetails(int? Id);
+        public IActionResult Confirm(int? Id);
+
+    }
+}
